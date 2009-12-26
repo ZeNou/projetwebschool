f2c75188eebb09adf12a38c407b0df1c9d150da6
dizzda <dizzda@375d1d56-efdc-11de-a8d0-a3f7a2b5467f>
2009-12-26 15:01:14 +0000

    mise en place du design/css de fred

--- index.php
+++ index.php
@@ -29,8 +29,9 @@ if (isset($_GET['d']) && isset($_SESSION['id'])) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>dizSurf</title>
+	<title>ProjetWeb</title>
 	
 	<link href="css/general.css" rel="stylesheet" type="text/css" />
 	
@@ -39,13 +40,53 @@ if (isset($_GET['d']) && isset($_SESSION['id'])) {
 </head>
 
 <body>
-	
-	<div id="page">
+
+	<div id="contenu">
+
+		<div id="header">
+			<ul>
+				<li><a href="index.php?page=accueil">Accueil</a></li>
+				<li><a href="index.php?page=inscription">Inscription</a></li>
+				<li><a href="index.php?page=login">Login</a></li>
+			</ul>
+		</div>
 		
-		<?php include(dirname(__FILE__). '/centre.php'); ?>
+		<dl id="menu">
+			<dt><a href="index.php?page=article&amp;rubrique=1">Rubrique 1</a></dt>
+			<dd>
+				<ul>
+					<li><a href="index.php?page=article&amp;article=1">article 1.1</a></li>
+					<li><a href="index.php?page=article&amp;article=2">article 1.2</a></li>
+					<li><a href="index.php?page=article&amp;article=3">article 1.3</a></li>
+				</ul>
+			</dd>
+			<dt><a href="index.php?page=article&amp;rubrique=2">Rubrique 2</a></dt>
+			<dd>
+				<ul>
+					<li><a href="index.php?page=article&amp;article=4">article 2.1</a></li>
+					<li><a href="index.php?page=article&amp;article=5">article 2.2</a></li>
+					<li><a href="index.php?page=article&amp;article=6">article 2.3</a></li>
+				</ul>
+			</dd>
+			<dt><a href="index.php?page=article&amp;rubrique=3">Rubrique 3</a></dt>
+			<dd>
+				<ul>
+					<li><a href="index.php?page=article&amp;article=7">article 3.1</a></li>
+					<li><a href="index.php?page=article&amp;article=8">article 3.2</a></li>
+					<li><a href="index.php?page=article&amp;article=9">article 3.3</a></li>
+				</ul>
+			</dd>
+		</dl>
+		
+		<div id="page">
+			
+			<?php include(dirname(__FILE__). '/centre.php'); ?>
+			
+		</div>
+		
+		<p id="footer">&copy; <?php echo date("Y"); ?>, INSIA - <a href="index.php?page=contact">Contact</a></p>
 		
 	</div>
-	
-	
+		
 </body>
 </html>
