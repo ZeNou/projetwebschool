da9922dca4a140b185aa06d532b5c8bf1470434c
benjamin.leboucq <benjamin.leboucq@375d1d56-efdc-11de-a8d0-a3f7a2b5467f>
2009-12-29 18:55:02 +0000

    liste des catégories (modif à faire sur mm fichies)

---
+++ inc/modifcategorie.inc.php
@@ -0,0 +1,32 @@
+﻿<?php
+if($_SESSION['level'] == 9)
+{
+	echo '<h1>Ajouter une nouvelle catégorie</h1><br />' ;
+
+	$list_cat = new Sql();
+			
+	$tab_cat = Tab($list_cat,'	SELECT id, nom
+								FROM categorie ; ');
+			
+	echo '<table>
+			<tr>
+				<th>ID</th>
+				<th>Nom de la catégorie</th>
+			</tr>' ;
+	foreach ($tab_cat as $tab_msg)
+	{
+		echo '<tr>
+				<td>'.$tab_msg['id'].'</td>
+				<td>'.$tab_msg['nom'].'</td>
+			</tr>';
+	}
+	echo '</table>';
+}
+else
+{
+	echo '<br /><br />
+			<ul class="erreur">
+				<li>Vous n\'avez pas accès à cette page</li>
+			</ul>' ;
+}
+?>
