120153a5a5927786afc9b3c2910cfaa53a13727a
dizzda <dizzda@375d1d56-efdc-11de-a8d0-a3f7a2b5467f>
2010-01-02 15:28:36 +0000

    maj menu

--- trunk/box/menu.php
+++ trunk/box/menu.php
@@ -25,7 +25,7 @@ if(!isset($_SESSION['id']))
 				<dt>Votre compte : </dt>
 					<dd>
 						<ul>
-							<li><a href="index.php?page=">Gerer vos amis</a></li>
+							<li><a href="index.php?page=listamis">Gerer vos amis</a></li>
 							<li><a href="index.php?page=">Modifier les informations de votre compte</a></li>
 							<li><a href="index.php?page=">Modifier votre mot de passe</a></li>
 						</ul>
