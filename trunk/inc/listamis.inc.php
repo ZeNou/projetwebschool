afb23f0850cfdd729a58f5cf1377f038c0fc0b71
dizzda <dizzda@375d1d56-efdc-11de-a8d0-a3f7a2b5467f>
2010-01-02 17:24:24 +0000

    listamis.inc.php

---
+++ trunk/inc/listamis.inc.php
@@ -0,0 +1,52 @@
+<?php
+if(!isset($_SESSION['id']))
+{
+
+	echo errAcces();
+	exit();
+				
+}
+
+	echo '<h1>Liste de vos amis</h1><br />' ;
+
+	echo '<dl>';
+	
+	$friends 	= new Sql();
+			
+	$tab_cat 	= Tab($friends,'SELECT DISTINCT g.id, g.titre
+											FROM '.tblgroupe.' g,'.tblamis.' a
+											WHERE a.id_groupe_amis = g.id
+											');
+			
+
+		foreach ($tab_cat as $grp)
+		{
+			$id_groupe = $grp['id'];
+			
+			echo '<dt>'.$grp['titre'].'</dt>
+						<dd>
+								<ul>
+					';
+			
+			
+			$tab_amis 	= Tab($friends,'SELECT m.id, m.pseudo
+														FROM '.tblamis.' a, '.tblmembres.' m
+														WHERE a.id_amis = m.id
+														AND a.id_groupe_amis = \''.$id_groupe.'\'
+														');
+								
+								
+			foreach ($tab_amis as $amis)
+			{
+				echo '<li>- '.$amis['pseudo'].' <span class="suppr">(<a href="#">supprimer</a>)</span></li>';
+			}
+			
+			
+			echo '	</ul>
+					</dd>';
+		}
+		
+	echo '</dl>';
+
+	
+?>
