14155f1e5c8c0f44e9d41951eee32b9edcf7cde7
benjamin.leboucq <benjamin.leboucq@375d1d56-efdc-11de-a8d0-a3f7a2b5467f>
2010-01-02 23:08:28 +0000

    liste des textes modif (ajout liste from bdd) mais erreur avec la date dans la bdd.... à look

--- trunk/inc/liretext.inc.php
+++ trunk/inc/liretext.inc.php
@@ -9,54 +9,65 @@ if(isset($_GET['idtext']))
 	}
 	else
 	{
-		$idtext = base64_decode($_GET['idtext']) ;
-		$auteur = base64_decode($_GET['aut']) ;
-		$categorie = base64_decode($_GET['cat']) ;
-		
-		$MyDirectory = opendir('user_txt');
-		
-		while($file = @readdir($MyDirectory)) 
+		$typefichier = base64_decode($_GET['type']) ;
+	
+		if($typefichier == 'fichier')
 		{
-			if(!is_dir('user_txt'.'/'.$file) AND $file != '.' AND $file != '..') 
+			$idtext = base64_decode($_GET['idtext']) ;
+			$auteur = base64_decode($_GET['aut']) ;
+			$categorie = base64_decode($_GET['cat']) ;
+			
+			$MyDirectory = opendir('user_txt');
+			
+			while($file = @readdir($MyDirectory)) 
 			{
-				$search_txt = explode('#' , $file);
-				$extension = explode('.', $search_txt[8]) ;
-				if($extension[0] == $idtext)
+				if(!is_dir('user_txt'.'/'.$file) AND $file != '.' AND $file != '..') 
 				{
-					echo '
-					<table id="liretxt">
-						<tr>
-							<th> Auteur </th>
-							<th> Date d\'ajout </th>
-							<th> Catégorie </th>
-						</tr>
-						<tr>
-							<td class="td_listtxt"> '.$auteur.' </td>
-							<td class="td_listtxt"> '.date("\L\e j/n/Y à H:i:s", $search_txt[6]).' </td>
-							<td class="td_listtxt"> '.$categorie.' </td>
-						</tr>
-					</table><br /><br />';
-					
-					echo '
-					<fieldset id=affichagetxt>
-							<legend>'.$search_txt[2].'</legend> ';
-							if (!$ouverturefile = fopen('user_txt/'.$file.'','r')) 
-							{
-								echo 'Echec de l\'ouverture du fichier';
-								exit;
-							}
-							else 
-							{
-								$contenutxt = file_get_contents('/user_txt/'.$file.'', FILE_USE_INCLUDE_PATH);
-								echo '<div id="corpsdutxt">'.$contenutxt.'</div>' ;
-								fclose($ouverturefile); 
-							}
-					echo '
-					</fieldset>';
+					$search_txt = explode('#' , $file);
+					$extension = explode('.', $search_txt[8]) ;
+					if($extension[0] == $idtext)
+					{
+						echo '
+						<table id="liretxt">
+							<tr>
+								<th> Auteur </th>
+								<th> Date d\'ajout </th>
+								<th> Catégorie </th>
+								<th> Etat du texte </th>
+							</tr>
+							<tr>
+								<td class="td_listtxt"> '.$auteur.' </td>
+								<td class="td_listtxt"> '.date("\L\e j/n/Y à H:i:s", $search_txt[6]).' </td>
+								<td class="td_listtxt"> '.$categorie.' </td>
+								<td class="td_listtxt"> Non modéré pas l\'administrateur </td>
+							</tr>
+						</table><br /><br />';
+						
+						echo '
+						<fieldset id=affichagetxt>
+								<legend>'.$search_txt[2].'</legend> ';
+								if (!$ouverturefile = fopen('user_txt/'.$file.'','r')) 
+								{
+									echo 'Echec de l\'ouverture du fichier';
+									exit;
+								}
+								else 
+								{
+									$contenutxt = file_get_contents('/user_txt/'.$file.'', FILE_USE_INCLUDE_PATH);
+									echo '<div id="corpsdutxt">'.$contenutxt.'</div>' ;
+									fclose($ouverturefile); 
+								}
+						echo '
+						</fieldset>';
+					}
 				}
 			}
+			closedir($MyDirectory);
 		}
-		closedir($MyDirectory);
+		elseif($typefichier == 'bdd')
+		{
+			
+		}	
 	}
 }
 ?>
--- trunk/inc/listtext.inc.php
+++ trunk/inc/listtext.inc.php
@@ -20,7 +20,8 @@ if(isset($_SESSION['id']))
 	</form>
 	
 	<br />
-	<table id="listtext">
+	<table class="listtext">
+		<caption> Texte non modéré par l\'administrateur </caption>
 		<tr>
 			<th rowspan="2"> Auteur </th>
 			<th rowspan="2"> Catégorie </th>
@@ -50,73 +51,155 @@ if(isset($_SESSION['id']))
 			$valeur = explode("#", $file) ;
 			$extension = explode(".", $valeur[8]) ;
 			
-			$search_cat = new Sql();
-			
-			$tab_cat = Tab($search_cat,'	SELECT nom
-											FROM categorie  
-											WHERE id='.$valeur[1].'');
-			$idcat = $valeur[1] ;
-			$valeur[1] = $tab_cat[0]['nom'];
-			
-			$search_auteur = new Sql();
-			
-			$tab_auteur = Tab($search_auteur,'	SELECT pseudo
-												FROM membre  
-												WHERE id='.$valeur[0].'');
-			$valeur[0] = $tab_auteur[0]['pseudo'];
-			
-			if($valeur[3] == 1) $valeur[3] = "oui"; else $valeur[3] = "non" ;
-			if($valeur[4] == 1) $valeur[4] = "oui"; else $valeur[4] = "non" ;
-			if($valeur[5] == 1) $valeur[5] = "oui"; else $valeur[5] = "non" ;
-			
-			if(isset($_POST['valid_tricat']) AND ($_POST['add_txtcat'] != 'ALL'))
+			if($valeur[7] == 'false')
 			{
-				$idcatok = base64_decode($_POST['add_txtcat']) ;
-
-				if($idcatok == $idcat)
-				{
+				$search_cat = new Sql();
 				
-					echo '
-						<tr> 
-							<td> '.$valeur[0].' </td>
-							<td> '.$valeur[1].' </td>
-							<td> '.$valeur[2].' </td>
-							<td class="td_listtxt"> '.date("\L\e j/n/Y à H:i:s", $valeur[6]).' </td>
-							<td class="td_listtxt"> '.$valeur[3].' </td>
-							<td class="td_listtxt"> '.$valeur[4].' </td>
-							<td class="td_listtxt"> '.$valeur[5].' </td>
-							<td class="td_listtxt"> <a href="index.php?page=liretext&idtext='.base64_encode($extension[0]).'&aut='.base64_encode($valeur[0]).'&cat='.base64_encode($valeur[1]).'" title="Lecture du texte"><img src="./images/png/read.png" alt="lecture" /> </td> ';
+				$tab_cat = Tab($search_cat,'	SELECT nom
+												FROM categorie  
+												WHERE id='.$valeur[1].'');
+				$idcat = $valeur[1] ;
+				$valeur[1] = $tab_cat[0]['nom'];
+				
+				$search_auteur = new Sql();
+				
+				$tab_auteur = Tab($search_auteur,'	SELECT pseudo
+													FROM membre  
+													WHERE id='.$valeur[0].'');
+				$valeur[0] = $tab_auteur[0]['pseudo'];
+				
+				if($valeur[3] == 1) $valeur[3] = "oui"; else $valeur[3] = "non" ;
+				if($valeur[4] == 1) $valeur[4] = "oui"; else $valeur[4] = "non" ;
+				if($valeur[5] == 1) $valeur[5] = "oui"; else $valeur[5] = "non" ;
+				
+				if(isset($_POST['valid_tricat']) AND ($_POST['add_txtcat'] != 'ALL'))
+				{
+					$idcatok = base64_decode($_POST['add_txtcat']) ;
+
+					if($idcatok == $idcat)
+					{
+					
+						echo '
+							<tr> 
+								<td> '.$valeur[0].' </td>
+								<td> '.$valeur[1].' </td>
+								<td> '.$valeur[2].' </td>
+								<td class="td_listtxt"> '.date("\L\e j/n/Y à H:i:s", $valeur[6]).' </td>
+								<td class="td_listtxt"> '.$valeur[3].' </td>
+								<td class="td_listtxt"> '.$valeur[4].' </td>
+								<td class="td_listtxt"> '.$valeur[5].' </td>
+								<td class="td_listtxt"> <a href="index.php?page=liretext&idtext='.base64_encode($extension[0]).'&aut='.base64_encode($valeur[0]).'&cat='.base64_encode($valeur[1]).'&type='.base64_encode('fichier').'" title="Lecture du texte"><img src="./images/png/read.png" alt="lecture" /> </td> ';
+						if($_SESSION['level'] == 9)
+						{
+							echo '	<td class="td_listtxt"> <a href="index.php?page=supptext&idtext='.base64_encode($extension[0]).'&type='.base64_encode('fichier').'" title="Supprimé ce texte"><img src="./images/png/supp.png" alt="suppression" /> </td>
+									<td class="td_listtxt"> <a href="index.php?page=addtobdd&idtext='.base64_encode($extension[0]).'&type='.base64_encode('fichier').'" title="Ajouté à la BDD"><img src="./images/png/save.png" alt="ajouté à la bdd" /> </td> ';
+						}
+						echo'	</tr>' ;
+					}
+				}
+				else
+				{
+						echo '
+							<tr> 
+								<td> '.$valeur[0].' </td>
+								<td> '.$valeur[1].' </td>
+								<td> '.$valeur[2].' </td>
+								<td class="td_listtxt"> '.date("\L\e j/n/Y à H:i:s", $valeur[6]).' </td>
+								<td class="td_listtxt"> '.$valeur[3].' </td>
+								<td class="td_listtxt"> '.$valeur[4].' </td>
+								<td class="td_listtxt"> '.$valeur[5].' </td>
+								<td class="td_listtxt"> <a href="index.php?page=liretext&idtext='.base64_encode($extension[0]).'&aut='.base64_encode($valeur[0]).'&cat='.base64_encode($valeur[1]).'&type='.base64_encode('fichier').'" title="Lecture du texte"><img src="./images/png/read.png" alt="lecture" /> </td> ';
 					if($_SESSION['level'] == 9)
 					{
-						echo '	<td class="td_listtxt"> <a href="index.php?page=supptext&idtext='.base64_encode($extension[0]).'" title="Supprimé ce texte"><img src="./images/png/supp.png" alt="suppression" /> </td>
-								<td class="td_listtxt"> <a href="index.php?page=addtobdd&idtext='.base64_encode($extension[0]).'" title="Ajouté à la BDD"><img src="./images/png/save.png" alt="ajouté à la bdd" /> </td> ';
+						echo '	<td class="td_listtxt"> <a href="index.php?page=supptext&idtext='.base64_encode($extension[0]).'&type='.base64_encode('fichier').'" title="Supprimé ce texte"><img src="./images/png/supp.png" alt="suppression" /> </td>
+								<td class="td_listtxt"> <a href="index.php?page=addtobdd&idtext='.base64_encode($extension[0]).'&type='.base64_encode('fichier').'" title="Ajouté à la BDD"><img src="./images/png/save.png" alt="ajouté à la bdd" /> </td> ';
 					}
 					echo'	</tr>' ;
 				}
 			}
-			else
-			{
-					echo '
-						<tr> 
-							<td> '.$valeur[0].' </td>
-							<td> '.$valeur[1].' </td>
-							<td> '.$valeur[2].' </td>
-							<td class="td_listtxt"> '.date("\L\e j/n/Y à H:i:s", $valeur[6]).' </td>
-							<td class="td_listtxt"> '.$valeur[3].' </td>
-							<td class="td_listtxt"> '.$valeur[4].' </td>
-							<td class="td_listtxt"> '.$valeur[5].' </td>
-							<td class="td_listtxt"> <a href="index.php?page=liretext&idtext='.base64_encode($extension[0]).'&aut='.base64_encode($valeur[0]).'&cat='.base64_encode($valeur[1]).'" title="Lecture du texte"><img src="./images/png/read.png" alt="lecture" /> </td> ';
+		}
+	}
+	echo '</table><br /><br />' ;
+	
+	closedir($MyDirectory);
+	
+	echo '
+	<table class="listtext">
+		<caption> Texte modéré par l\'administrateur </caption>
+		<tr>
+			<th rowspan="2"> Auteur </th>
+			<th rowspan="2"> Catégorie </th>
+			<th rowspan="2"> Titre du texte </th>
+			<th rowspan="2"> Date d\'ajout </th>
+			<th colspan="3"> Autorisation </th>		
+			<th rowspan="2"> Lire ce texte </th> ';
+		if($_SESSION['level'] == 9)
+		{
+			echo '
+			<th rowspan="2"> Delete </th> ';
+		}
+	echo '
+		</tr>
+		<tr>
+			<th>Lecture</th>
+			<th>Notation</th>
+			<th>Commentaire</th>
+		</tr>';
+					
+	$affiche_txt = new Sql();
+	
+	$tab_affichetxt = Tab($affiche_txt,'SELECT m.pseudo, c.nom, t.titre, t.date_ajout, t.droit_lecture, t.droit_notation, t.droit_commenter, t.id_categorie, t.id
+										FROM membre m JOIN texte t ON(t.id_membre=m.id) JOIN categorie c ON(c.id=t.id_categorie) ');
+									
+	foreach ($tab_affichetxt as $tab_msg)
+	{
+		if($tab_msg['droit_lecture'] == 1) $tab_msg['droit_lecture']  = "oui"; else $tab_msg['droit_lecture']  = "non" ;
+		if($tab_msg['droit_notation'] == 1) $tab_msg['droit_notation']  = "oui"; else $tab_msg['droit_notation']  = "non" ;
+		if($tab_msg['droit_commenter'] == 1) $tab_msg['droit_commenter']  = "oui"; else $tab_msg['droit_commenter']  = "non" ;
+		
+		if(isset($_POST['valid_tricat']) AND ($_POST['add_txtcat'] != 'ALL'))
+		{
+			$idcatok = base64_decode($_POST['add_txtcat']) ;
+
+			if($idcatok == $tab_msg['id_categorie'])
+			{			
+				echo '
+					<tr> 
+						<td> '.$tab_msg['pseudo'].' </td>
+						<td> '.$tab_msg['nom'].' </td>
+						<td> '.$tab_msg['titre'].' </td>
+						<td class="td_listtxt"> '.date("\L\e j/n/Y à H:i:s", $tab_msg['date_ajout']).' </td>
+						<td class="td_listtxt"> '.$tab_msg['droit_lecture'].' </td>
+						<td class="td_listtxt"> '.$tab_msg['droit_notation'].' </td>
+						<td class="td_listtxt"> '.$tab_msg['droit_commenter'].' </td>
+						<td class="td_listtxt"> <a href="index.php?page=liretext&idtext='.base64_encode($tab_msg['id']).'&aut='.base64_encode($tab_msg['pseudo']).'&cat='.base64_encode($tab_msg['nom']).'&type='.base64_encode('bdd').'" title="Lecture du texte"><img src="./images/png/read.png" alt="lecture" /> </td> ';
 				if($_SESSION['level'] == 9)
 				{
-					echo '	<td class="td_listtxt"> <a href="index.php?page=supptext&idtext='.base64_encode($extension[0]).'" title="Supprimé ce texte"><img src="./images/png/supp.png" alt="suppression" /> </td>
-							<td class="td_listtxt"> <a href="index.php?page=addtobdd&idtext='.base64_encode($extension[0]).'" title="Ajouté à la BDD"><img src="./images/png/save.png" alt="ajouté à la bdd" /> </td> ';
+					echo '	<td class="td_listtxt"> <a href="index.php?page=supptext&idtext='.base64_encode($tab_msg['id']).'&type='.base64_encode('bdd').'" title="Supprimé ce texte"><img src="./images/png/supp.png" alt="suppression" /> </td> ';
 				}
 				echo'	</tr>' ;
 			}
 		}
+		else
+		{
+			echo '
+				<tr> 
+					<td> '.$tab_msg['pseudo'].' </td>
+					<td> '.$tab_msg['nom'].' </td>
+					<td> '.$tab_msg['titre'].' </td>
+					<td class="td_listtxt"> '.date("\L\e j/n/Y à H:i:s", $tab_msg['date_ajout']).' </td>
+					<td class="td_listtxt"> '.$tab_msg['droit_lecture'].' </td>
+					<td class="td_listtxt"> '.$tab_msg['droit_notation'].' </td>
+					<td class="td_listtxt"> '.$tab_msg['droit_commenter'].' </td>
+					<td class="td_listtxt"> <a href="index.php?page=liretext&idtext='.base64_encode($tab_msg['id']).'&aut='.base64_encode($tab_msg['pseudo']).'&cat='.base64_encode($tab_msg['nom']).'&type='.base64_encode('bdd').'" title="Lecture du texte"><img src="./images/png/read.png" alt="lecture" /> </td> ';
+			if($_SESSION['level'] == 9)
+			{
+				echo '	<td class="td_listtxt"> <a href="index.php?page=supptext&idtext='.base64_encode($tab_msg['id']).'&type='.base64_encode('bdd').'" title="Supprimé ce texte"><img src="./images/png/supp.png" alt="suppression" /> </td> ';
+			}
+			echo'	</tr>' ;
+		}
+		
 	}
 	echo '</table>' ;
-	
-	closedir($MyDirectory);
 }
 ?>
