768e12e6e289f771bb2ccf15d432829b6d109c3a
benjamin.leboucq <benjamin.leboucq@375d1d56-efdc-11de-a8d0-a3f7a2b5467f>
2010-01-10 02:50:13 +0000

    a finir modif droit pour user by cat + option

--- trunk/inc/modifdroit.inc.php
+++ trunk/inc/modifdroit.inc.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 if(isset($_SESSION['id']))
 {	
 	if(isset($_GET['idmem']))
@@ -11,20 +11,51 @@ if(isset($_SESSION['id']))
 												FROM droitbycategorie 
 												WHERE membre_id = '.$idok.'');
 		
-		if(count($tab_autorisation) == 0)
+		if(count($tab_autorisation) != 0)
 		{
-			echo 'aucune restriction' ;
+			echo '
+				<table>
+					<tr>
+						<th>';
+								if($tab_autorisation == 1)
+									echo 'Ce membre ne peux pas poster de texte dans la catégorie : ' ;
+								else
+									echo 'Ce membre ne peux pas poster de texte dans les catégories : ' ;
+			echo '		</th>
+					</tr>
+					<tr>
+						<td>';			
+							foreach($tab_autorisation as $msg_autorisation)
+							{
+								if($msg_autorisation['membre_id'] == $idok)
+								{				
+									$catname = new Sql();
+						
+									$tab_catname = Tab($catname,'	SELECT nom
+																	FROM categorie 
+																	WHERE id = '.$msg_autorisation['categorie_id'].' ');
+									
+									foreach($tab_catname as $msg_catname)
+									{
+										echo ' - '.$msg_catname['nom'].' <br />' ;
+									}					
+								}	
+							}
+			echo '		</td>
+					</tr>
+				</table> <br /> <br />';
 		}
 		else
 		{
-			foreach($tab_autorisation as $msg_autorisation)
-			{
-				if($msg_autorisation['membre_id'] == $idok)
-				{
-					echo 'restriction ok' ;
-				}	
-			}
+			echo '
+				<table>
+					<tr>
+						<th> Ce membre n\'a pas de restrictions. </th>
+					</tr>
+				</table> <br /> <br />';
 		}
+		
+		echo 'formulaire des modifications des restrictions';
 	}
 }
 ?>
