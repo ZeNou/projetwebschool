f406400bc94845c64de38fb8fd5fe42da7e5b449
dizzda <dizzda@375d1d56-efdc-11de-a8d0-a3f7a2b5467f>
2010-01-10 23:35:20 +0000

    Gestion des droits des membres pr les catégories ok

--- inc/modifdroit.inc.php
+++ inc/modifdroit.inc.php
@@ -3,14 +3,44 @@ if(isset($_SESSION['id']))
 {	
 	if(isset($_GET['idmem']))
 	{
-		$idok = base64_decode($_GET['idmem']) ;
-		
 		$autorisation = new Sql();
 		
+		$idok = base64_decode($_GET['idmem']);
+		
+		/*
+		 * Supprime une interdiction
+		 */
+		 
+		if(isset($_GET['idsuppr']))
+		{
+			
+			$id_suppr = base64_decode($_GET['idsuppr']);
+		
+			$suppr_interdit = Req($autorisation,'DELETE FROM droitbycategorie
+																WHERE membre_id = '.$idok.'
+																AND categorie_id = '.$id_suppr.'') ;
+		}
+		
+		/*
+		 *	Ajoute l'interdiction a une catégorie
+		 */
+		if(isset($_POST['categorie']))
+		{
+			foreach($_POST['categorie'] as $interdit)
+			{
+				
+				$ajout_interdit = Req($autorisation,'INSERT INTO droitbycategorie (membre_id, categorie_id)
+																	VALUES ('.$idok.','.$interdit.') ') ;
+				
+			}
+		}
+		
 		$tab_autorisation = Tab($autorisation,'	SELECT *
-												FROM droitbycategorie 
-												WHERE membre_id = '.$idok.'');
-		echo '<div id="restrictionbycat">';
+																	FROM droitbycategorie 
+																	WHERE membre_id = '.$idok.'');
+		echo '<div id="restrictionbycat">
+				<form method="POST" action="" id="form_modifdroit">';
+				
 		if(count($tab_autorisation) != 0)
 		{
 			echo '
@@ -28,16 +58,16 @@ if(isset($_SESSION['id']))
 							foreach($tab_autorisation as $msg_autorisation)
 							{
 								if($msg_autorisation['membre_id'] == $idok)
-								{				
+								{
 									$catname = new Sql();
 						
 									$tab_catname = Tab($catname,'	SELECT nom
-																	FROM categorie 
-																	WHERE id = '.$msg_autorisation['categorie_id'].' ');
+																					FROM categorie 
+																					WHERE id = '.$msg_autorisation['categorie_id'].' ');
 									
 									foreach($tab_catname as $msg_catname)
 									{
-										echo ' - '.$msg_catname['nom'].' <br />' ;
+										echo ' - '.$msg_catname['nom'].' <span class="suppr">(<a href="index.php?page=modifdroit&idmem='.base64_encode($idok).'&idsuppr='.base64_encode($msg_autorisation['categorie_id']).'">supprimer</a>)</span><br />' ;
 									}					
 								}	
 							}
@@ -48,7 +78,6 @@ if(isset($_SESSION['id']))
 		else
 		{
 			echo '
-				<form method="POST" action="" id="form_modifdroit">
 				<table>
 					<tr>
 						<th> Ce membre n\'a pas de restrictions. </th>
@@ -58,8 +87,13 @@ if(isset($_SESSION['id']))
 		
 		$listecat = new Sql();
 		
-		$tab_listecat = Tab($listecat,'	SELECT *
-										FROM categorie ');
+		/* Sous jointure qui permet de ne pas ressortir les catégories déjà interdites */
+		$tab_listecat = Tab($listecat,'	SELECT id, nom
+													FROM categorie
+													WHERE id NOT IN (SELECT categorie_id
+																				FROM droitbycategorie
+																				WHERE membre_id = '.$idok.')
+													');
 		
 		echo '
 			<table>
@@ -70,7 +104,7 @@ if(isset($_SESSION['id']))
 		{
 			echo '
 				<tr>
-					<td><input type="checkbox" value="'.$msg_listecat['id'].'" name="categorie" id="'.$msg_listecat['nom'].'" /> <label for="'.$msg_listecat['nom'].'"> '.$msg_listecat['nom'].' </label></td>
+					<td><input type="checkbox" value="'.$msg_listecat['id'].'" name="categorie[]" id="'.$msg_listecat['nom'].'" /> <label for="'.$msg_listecat['nom'].'"> '.$msg_listecat['nom'].' </label></td>
 				</tr>';
 		}
 		echo '	<tr>
